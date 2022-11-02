b37e74837fca4a97cef14d94035c3ade6e4a3d95
Akhmadjon <ahmadjonzuhriddinov84@gmail.com>
2022-11-02 23:08:42 +0900

    added some features to css for flex-displaying and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //  TYPING ANIMATION //
 var typed = new Typed(".typing", {
-    strings:["","Backend Engineer", "Web Developer", "MERN Stack Developer", "Student 👨🏻‍💻"],
+    strings:["","Backend Engineer", "Web Developer", "MERN Stack Developer", "Student 🧑‍🎓"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true
@@ -8,15 +8,51 @@ var typed = new Typed(".typing", {
 // Aside //
 const nav = document.querySelector(".nav"),
     navList = nav.querySelectorAll("li"),
-    totalNavList = navList.length;
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
     for(let i=0; i<totalNavList; i++)
     {
         const a = navList[i].querySelector("a");
-        a.addEventListener("click", function() {
+        a.addEventListener("click", function() 
+        {
+            for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.remove("back-section");
+            }
             for(let j=0; j<totalNavList; j++)
             {
+                if(navList[j].querySelector("a").classList.contains("active"))
+                {
+                    allSection[j].classList.add("back-section");
+                }
                 navList[j].querySelector("a").classList.remove("active");
             }
             this.classList.add("active")
+            showSection(this);
         })
     }
+    function showSection(element) 
+    {
+        for(let i=0; i<totalSection; i++)
+        {
+            allSection[i].classList.remove("active");
+        }
+       const target = element.getAttribute("href").split("#")[1];
+       document.querySelector("#" + target).classList.add("active")
+    }
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+          aside = document.querySelector(".aside");
+          navTogglerBtn.addEventListener("click", () => 
+          {
+            asideSectionTogglerBtn();
+          })
+          function asideSectionTogglerBtn()
+          {
+            aside.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+            for(let i=0; i<totalSection; i++)
+            {
+                allSection[i].classList.toggle("open")
+            }
+          }
